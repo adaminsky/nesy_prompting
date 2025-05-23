0784d23055881f3160527e837fd4399564102699
Neelay Velingker <nvelingker@gmail.com>
2025-05-23 04:33:14 -0400

    corrected leaf prsy logs and using newwr OurLLM/APIModel

--- scripts/llm_symbolic_eval.py
+++ scripts/llm_symbolic_eval.py
@@ -16,6 +16,7 @@ from src.utils import IOExamples, RawInput, img2base64, base642img, eval_extract
 from src.pddl import eval_solution_files, find_solution
 from src.function_evaluation import python_eval
 from src.program_gen import ProgramSynthesisSolver, DSLOp
+from src.llm_models import OurLLM, APIModel
 import time
 import itertools
 import json
@@ -28,101 +29,6 @@ from time import sleep
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-
-class OurLLM:
-    def __init__(self, model_name):
-        self.model_name = model_name
-        if "Llama-3.2" in model_name:
-            self.model = MllamaForConditionalGeneration.from_pretrained(model_name, torch_dtype="auto", device_map="auto", token="***REMOVED***",)
-            self.processor = AutoProcessor.from_pretrained(model_name, token="***REMOVED***")
-        elif "QVQ" in model_name:
-            self.model = Qwen2VLForConditionalGeneration.from_pretrained(model_name, torch_dtype="auto", device_map="auto", attn_implementation="flash_attention_2", token="***REMOVED***")
-            self.processor = AutoProcessor.from_pretrained(model_name, token="***REMOVED***")
-        elif "Qwen" in model_name:
-            self.model = Qwen2_5_VLForConditionalGeneration.from_pretrained(model_name, torch_dtype=torch.bfloat16, device_map="auto", attn_implementation="flash_attention_2", token="***REMOVED***")
-            self.processor = AutoProcessor.from_pretrained(model_name, token="***REMOVED***")
-        elif "OpenGVLab/InternVL2_5-78B-MPO" in model_name:
-            self.model = AutoModel.from_pretrained(model_name, torch_dtype="auto", device_map="auto", attn_implementation="flash_attention_2", token="***REMOVED***", trust_remote_code=True)
-            self.processor = AutoProcessor.from_pretrained(model_name, token="***REMOVED***", trust_remote_code=True)
-        elif "paligemma" in model_name:
-            self.model = PaliGemmaForConditionalGeneration.from_pretrained(model_name, torch_dtype=torch.bfloat16, device_map="auto").eval()
-            self.processor = PaliGemmaProcessor.from_pretrained(model_name)
-
-    def chat(self, prompt, sampling_params, use_tqdm):
-        # parse prompt content
-        imgs = []
-        processed_prompt = []
-        for j in range(len(prompt)):
-            prompt_content = []
-            for i in range(len(prompt[j]["content"])):
-                if prompt[j]["content"][i]["type"] == "text":
-                    prompt_content.append(prompt[j]["content"][i])
-                elif prompt[j]["content"][i]["type"] == "image_url":
-                    prompt_content.append({"type": "image"})
-                    img_base64 = prompt[j]["content"][i]["image_url"]["url"].split(",")[1]
-                    imgs.append(base642img(img_base64))
-            processed_prompt.append({"role": prompt[j]["role"], "content": prompt_content})
-
-        # prompt = [{"role": "user", "content": prompt_content}]
-        print("prompt:", processed_prompt)
-
-        input_text = self.processor.apply_chat_template(processed_prompt, add_generation_prompt=True)
-        inputs = self.processor(imgs if len(imgs) > 0 else None, input_text, add_special_tokens=False, return_tensors="pt").to("cuda:0")
-
-        print(self.processor.decode(inputs["input_ids"][0]))
-        start = time.time()
-        with torch.no_grad():
-            outputs = self.model.generate(**inputs, max_new_tokens=2500, temperature=0.0, do_sample=False, top_p=1.0)
-        elapsed = time.time() - start
-
-        print(f"Tokens per second: {(len(outputs[0][len(inputs['input_ids'][0]):])) / elapsed}")
-
-        output_text = self.processor.decode(outputs[0][len(inputs["input_ids"][0]):][:-1])
-        print("output:", output_text)
-
-        class Outputs:
-            def __init__(self, outputs):
-                self.outputs = outputs
-
-        class Text:
-            def __init__(self, text):
-                self.text = text
-
-        return [Outputs([Text(output_text)])]
-
-
-class APIModel:
-    def __init__(self, model_name):
-        self.model_name = model_name
-        if "gemini" in model_name:
-            self.client = OpenAI(
-                api_key="***REMOVED***",
-                base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
-            )
-        else:
-            self.client = OpenAI(api_key="***REMOVED***")
-
-    def chat(self, prompt, sampling_params, use_tqdm):
-        response = self.client.chat.completions.create(
-            model=self.model_name,
-            messages=prompt,
-            temperature=0.0,
-            max_tokens=2500,
-            top_p=1.0,
-        )
-        class Outputs:
-            def __init__(self, outputs):
-                self.outputs = outputs
-
-        class Text:
-            def __init__(self, text):
-                self.text = text
-
-        # print number of tokens
-        print("Prompt tokens:", response.usage.prompt_tokens)
-        print("Response tokens:", response.usage.completion_tokens)
-
-        return [Outputs([Text(response.choices[0].message.content)])]
 
 
 
@@ -2579,7 +2485,7 @@ def main(args):
     #     args.model, torch_dtype=torch.bfloat16
     # ).to("cuda:0")
     # processor = AutoProcessor.from_pretrained(args.model)
-    if not args.use_hf and not "gemini" in args.model.lower() and not "gpt" in args.model.lower() and not "o3" in args.model.lower():
+    if not args.use_hf and not "gemini" in args.model.lower() and not "gpt" in args.model.lower() and not "o3" in args.model.lower() and not "qwen" in args.model.lower():
         extra_args = {}
         if "mistral" in args.model.lower():
             extra_args = {"config_format": "mistral", "load_format": "mistral", "tokenizer_mode": "mistral"}
@@ -2593,7 +2499,7 @@ def main(args):
             tensor_parallel_size=args.num_gpus,
             **extra_args
         )
-    elif "gemini" in args.model.lower() or "gpt" in args.model.lower() or "o3" in args.model.lower():
+    elif "gemini" in args.model.lower() or "gpt" in args.model.lower() or "o3" in args.model.lower() or "qwen" in args.model.lower():
         model = APIModel(args.model)
     else:
         model = OurLLM(model_name=args.model)
